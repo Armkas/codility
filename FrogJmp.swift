fbc1a23a553bb6e32488994e94ab2e75a22a2282
aPuYue <pu.yue@access-company.com>
2022-08-21 12:29:37 +0900

    FrogJmp

---
+++ FrogJmp.swift
@@ -0,0 +1,45 @@
+/*
+
+https://app.codility.com/demo/results/trainingXRFBZ8-DMP/
+
+A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+
+Count the minimal number of jumps that the small frog must perform to reach its target.
+
+Write a function:
+
+    public func solution(_ X : Int, _ Y : Int, _ D : Int) -> Int
+
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+
+For example, given:
+  X = 10
+  Y = 85
+  D = 30
+
+the function should return 3, because the frog will be positioned as follows:
+
+        after the first jump, at position 10 + 30 = 40
+        after the second jump, at position 10 + 30 + 30 = 70
+        after the third jump, at position 10 + 30 + 30 + 30 = 100
+
+Write an efficient algorithm for the following assumptions:
+
+        X, Y and D are integers within the range [1..1,000,000,000];
+        X ≤ Y.
+
+
+太简单了
+这题可以用循环试出来
+但是复杂度就是n
+用数学计算能大大减少复杂度
+这里是复杂度1
+*/
+
+public func solution(_ X : Int, _ Y : Int, _ D : Int) -> Int {
+    if (Y - X) % D == 0 {
+        return (Y - X) / D
+    } else {
+        return (Y - X) / D + 1
+    }
+}
